ff3655534ef5397c57d1ea5a75878e99d99dde0c
Thomas Fauve-Piot <tfauve-p@bess-f4r5s11.clusters.42paris.fr>
2025-02-05 12:12:21 +0100

    Map color ceiling & floor

--- SOURCES/parser_utils_3.c
+++ SOURCES/parser_utils_3.c
@@ -6,21 +6,29 @@ int	ft_is_valid_number(t_struct *data, char	*string, char *input, char c)
 
 	if (ft_strlen(input) >= 4)
 		return (BAD);
+	printf("input = %s\n", input);
 	nb = atoi(input);
+	printf("nb = %d\n", nb);
 	if (nb < 0 || nb > 255)
 		return (BAD);
-	if (ft_strncmp(string, "first", 5) == GOOD && c == 102)
-		data->floor_first = nb;
-	if (ft_strncmp(string, "second", 6) == GOOD && c == 102)
-		data->floor_second = nb;
-	if (ft_strncmp(string, "third", 5) == GOOD && c == 102)
-		data->floor_third = nb;
 	if (ft_strncmp(string, "first", 5) == GOOD && c == 99)
 		data->ceiling_first = nb;
 	if (ft_strncmp(string, "second", 6) == GOOD && c == 99)
 		data->ceiling_second = nb;
 	if (ft_strncmp(string, "third", 5) == GOOD && c == 99)
 		data->ceiling_third = nb;
+	if (ft_strncmp(string, "first", 5) == GOOD && c == 102)
+		data->floor_first = nb;
+	if (ft_strncmp(string, "second", 6) == GOOD && c == 102)
+		data->floor_second = nb;
+	if (ft_strncmp(string, "third", 5) == GOOD && c == 102)
+		data->floor_third = nb;
+	printf("C1 = %d\n", data->ceiling_first);
+	printf("C2 = %d\n", data->ceiling_second);
+	printf("C3 = %d\n", data->ceiling_third);
+	printf("F1 = %d\n", data->floor_first);
+	printf("F2 = %d\n", data->floor_second);
+	printf("F3 = %d\n", data->floor_third);
 	return (GOOD);
 }
 
@@ -73,11 +81,11 @@ int	ft_check_ceiling_values(t_struct *data, char *string)
 	first = ft_put_number_to_string(string, 0);
 	second = ft_put_number_to_string(string, 1);
 	third = ft_put_number_to_string(string, 2);
-	if (ft_is_valid_number(data, "first", first, 'f') == BAD)
+	if (ft_is_valid_number(data, "first", first, 'c') == BAD)
 		return (free(first), free(second), free(third), BAD);
-	if (ft_is_valid_number(data, "second", second, 'f') == BAD)
+	if (ft_is_valid_number(data, "second", second, 'c') == BAD)
 		return (free(first), free(second), free(third), BAD);
-	if (ft_is_valid_number(data, "third", third, 'f') == BAD)
+	if (ft_is_valid_number(data, "third", third, 'c') == BAD)
 		return (free(first), free(second), free(third), BAD);
 	return (free(first), free(second), free(third), GOOD);
 }
